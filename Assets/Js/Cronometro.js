43cbf795f0b9453d5ebc68126d1756cc2d324dc1
Wislley marques vieira dos santos <w52993000@gmail.com>
2024-06-21 17:23:30 -0300

    terminei

---
+++ Assets/Js/Cronometro.js
@@ -0,0 +1,88 @@
+let btn_iniciar_cronometro = document.querySelector('#btn_iniciar2')
+let btn_resetar_cronometro = document.querySelector('#btn_resetar2')
+let btn_parar_cronometro = document.querySelector('#btn_parar2')
+let segundos_cronometrados = document.querySelector('#segundos_cro')
+let minutos_cronometrados = document.querySelector('#minutos_cro')
+let horas_cronometrados = document.querySelector('#horas_cro')
+let dias_cronometrados = document.querySelector('#dias_cro')
+let tempo = 0
+let s = 0
+let m = 0
+let h = 0
+let d = 0
+
+btn_iniciar_cronometro.addEventListener('click', () => {
+    parar = false
+    btn_iniciar_cronometro.style.display = 'none'
+    btn_parar_cronometro.style.display = 'block'
+    if (!parar) {
+        intervalId = setInterval(() => {
+            tempo++
+            s++
+            if (s == 60) {
+                s = 0
+                m++
+            }
+
+            if (m == 60) {
+                m = 0
+                h++
+            }
+
+            if (h == 24) {
+                h = 0
+                d++
+            }
+
+            if (s <= 9) {
+                segundos_cronometrados.innerText = `0${s}s`
+            } else if (s > 9) {
+                segundos_cronometrados.innerText = `${s}s`
+            }
+
+            if (m <= 9) {
+                minutos_cronometrados.innerText = `0${m}m`
+            } else if (m > 9) {
+                minutos_cronometrados.innerText = `${m}m`
+            }
+
+            if (h <= 9) {
+                horas_cronometrados.innerText = `0${h}h`
+            } else if (h > 9) {
+                horas_cronometrados.innerText = `${h}h`
+            }
+
+            if (d <= 9) {
+                dias_cronometrados.innerText = `0${d}d`
+            } else if (d > 9) {
+                dias_cronometrados.innerText = `${d}d`
+            }
+
+        }, 1000);
+    } else {
+        clearInterval(intervalId)
+        parar = true
+    }
+})
+
+btn_parar_cronometro.addEventListener('click', () => {
+    clearInterval(intervalId)
+    btn_iniciar_cronometro.style.display = 'block'
+    btn_parar_cronometro.style.display = 'none'
+})
+
+btn_resetar_cronometro.addEventListener('click', () => {
+    clearInterval(intervalId)
+    btn_iniciar_cronometro.style.display = 'block'
+    btn_parar_cronometro.style.display = 'none'
+
+    s = 0
+    m = 0
+    h = 0
+    d = 0
+
+    segundos_cronometrados.innerText = `00s`
+    minutos_cronometrados.innerText = `00m`
+    horas_cronometrados.innerText = `00h`
+    dias_cronometrados.innerText = `00d`
+})
---
+++ Assets/Js/Timer.js
@@ -0,0 +1,73 @@
+let btn_iniciar = document.querySelector('#btn_iniciar')
+let btn_resetar = document.querySelector('#btn_resetar')
+let btn_parar = document.querySelector('#btn_parar')
+let span_minutos = document.querySelector('#minutos')
+let span_segundos = document.querySelector('#segundos')
+let timer = document.querySelector('#selected_timer')
+let cronometro = document.querySelector('#selected_cronometro')
+let section_timer = document.querySelector('#timer')
+let section_cronometro = document.querySelector('#cronometro')
+let parar = false
+let segundos = 300
+let intervalId // Variável para armazenar o identificador do intervalo
+
+timer.addEventListener('click', () => {
+    section_timer.style.display = 'none'
+    section_cronometro.style.display = 'flex'
+})
+
+cronometro.addEventListener('click', () => {
+    section_timer.style.display = 'flex'
+    section_cronometro.style.display = 'none'
+})
+
+btn_iniciar.addEventListener('click', () => {
+    parar = false
+    btn_iniciar.style.display = 'none'
+    btn_parar.style.display = 'block'
+
+    if (!parar) {
+        intervalId = setInterval(() => {
+            if (parar) {
+                clearInterval(intervalId) // Para o intervalo se 'parar' for true
+                return
+            }
+
+            segundos--
+            let minutos = parseInt(segundos / 60)
+            let seconds = segundos % 60
+
+            span_minutos.innerText = `${minutos}m`
+            span_segundos.innerText = seconds <= 9 ? `0${seconds}s` : `${seconds}s`
+
+            if (segundos <= 0) {
+                clearInterval(intervalId) // Para o intervalo quando o tempo acabar
+                parar = true
+                btn_iniciar.style.display = 'block'
+                btn_parar.style.display = 'none'
+            }
+        }, 1000);
+
+        setTimeout(() => {
+            parar = true
+            clearInterval(intervalId) // Garante que o intervalo pare após 300000 milissegundos (5 minutos)
+        }, 300000);
+    }
+})
+
+btn_resetar.addEventListener('click', () => {
+    segundos = 300
+    parar = true
+    clearInterval(intervalId) // Para o intervalo ao resetar
+    span_minutos.innerText = `5m`
+    span_segundos.innerText = `00s`
+    btn_iniciar.style.display = 'block'
+    btn_parar.style.display = 'none'
+})
+
+btn_parar.addEventListener('click', () => {
+    btn_iniciar.style.display = 'block'
+    btn_parar.style.display = 'none'
+    parar = true
+    clearInterval(intervalId) // Para o intervalo ao parar
+})
